29f23f5a69fa95fa5a967cab213cea4180397fda
Maya Rendon <poopxnose@gmail.com>
2016-08-05 10:02:30 -0500

    tweaked file

--- _2FINAL/_2FINAL.ino
+++ _2FINAL/_2FINAL.ino
@@ -1,48 +1,22 @@
-int LEDPIN= 3;
-int LEDPIN1= 13;
+int LEDPIN= 13;
+int LEDPIN1= 11;
+int LEDPIN2= 8;
+int LEDPIN3= 6;
 
 void setup() {
   pinMode(LEDPIN,OUTPUT);
   pinMode(LEDPIN1,OUTPUT);
+  pinMode(LEDPIN2,OUTPUT);
+  pinMode(LEDPIN3,OUTPUT);
+ 
+ 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    digitalWrite(LEDPIN, LOW);
-    digitalWrite(LEDPIN, HIGH);
-    delay(320);
+    digitalWrite(LEDPIN1, LOW);
     digitalWrite(LEDPIN1, HIGH);
     delay(320);
-    digitalWrite(LEDPIN1,LOW);
-    delay(340);
-    digitalWrite(LEDPIN1, HIGH);
-    delay(320);
-     digitalWrite(LEDPIN, LOW);
-    delay(340);
-    digitalWrite(LEDPIN1, LOW);
-    delay(340);
-    digitalWrite(LEDPIN1,HIGH);
-    delay(320);
-    digitalWrite(LEDPIN1, LOW);
-     
-     digitalWrite(LEDPIN, HIGH);
-    delay(350);
-    digitalWrite(LEDPIN1, HIGH);
-    delay(350);
-    digitalWrite(LEDPIN, LOW);
-    delay(360);
-    digitalWrite(LEDPIN1, LOW);
-    delay(320);
-    digitalWrite(LEDPIN, HIGH);
-    delay(360);
-    digitalWrite(LEDPIN, LOW); 
-    delay(320);
-    digitalWrite(LEDPIN1, HIGH);
-    delay(320);
-    digitalWrite(LEDPIN, HIGH);
-    delay(350);
-    digitalWrite(LEDPIN, LOW);
-    digitalWrite(LEDPIN1, LOW);
-   
-    delay(600);
+
+  
 }
